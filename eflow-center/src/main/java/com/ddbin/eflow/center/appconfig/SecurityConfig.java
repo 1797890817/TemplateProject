973d5301a1ac532714d74aff14877f17316d2716
1797890817 <1797890817@qq.com>
2017-08-09 17:44:09 +0800

    eflow-center add profile file Signed-off-by: 1797890817 <1797890817@qq.com>
    Signed-off-by: 1797890817 <1797890817@qq.com>

--- eflow-center/src/main/java/com/ddbin/eflow/center/appconfig/SecurityConfig.java
+++ eflow-center/src/main/java/com/ddbin/eflow/center/appconfig/SecurityConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
+    @Autowired(required = false)
     private UserRepository userRepository;
 
     @Override
